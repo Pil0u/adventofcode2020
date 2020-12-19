f2c9829ed8316c7d8c5c448d38a74f2190c0f4c0
Pierre-Loïc <pierreloic.pichon@gmail.com>
2020-12-19 19:43:42 +0000

    Drastically improved main script with better command line arguments parsing, inclunding options to choose the days to compute, whether you want to compute solutions and/or timings

--- run.py
+++ run.py
@@ -1,36 +1,82 @@
+import argparse
 import importlib
 import os
+import re
 import requests
 import sys
+import timeit
 
 from dotenv import load_dotenv
 
 load_dotenv()
 SESSION_COOKIE = os.getenv('SESSION_COOKIE')
+N_REPEATS = int(os.getenv('N_REPEATS')) if 'N_REPEATS' in os.environ else 1
 
 
-def compute(n):
-    f = importlib.import_module(f"day{n}")
+def get_args():
+    parser = argparse.ArgumentParser(description='Run and/or time daily challenges.')
+    parser.add_argument('-t', '--timers', dest='timers', action='store_true', help='get results along with timings')
+    parser.add_argument('-x', '--no-solutions', dest='solutions', action='store_false', help='get timings only')
+    parser.add_argument('-d', '--days', nargs='+', help='specify days to execute')
 
-    response = requests.get(f'https://adventofcode.com/2020/day/{n}/input', cookies={'session': SESSION_COOKIE})
+    parser.set_defaults(timers=False, solutions=True)
+
+    namespace = parser.parse_args()
+    namespace.timers = True if namespace.timers == False and namespace.solutions == False else namespace.timers
+
+    return namespace
+
+
+def preload(name):
+    f = importlib.import_module(f"{name}").result
+
+    number = int(re.search(r"\d+", name)[0])
+    response = requests.get(f'https://adventofcode.com/2020/day/{number}/input', cookies={'session': SESSION_COOKIE})
     input_ = response.content.decode('UTF-8').splitlines()
 
-    return f.result(input_)
+    return f, input_
+
+
+def timers(function, input_):
+    first_try = timeit.Timer(lambda: function(input_)).autorange()
+    n_run = first_try[0]
+    timers = [first_try[1]]
+    timers.extend(timeit.Timer(lambda: function(input_)).repeat(N_REPEATS-1, n_run))
+
+    return [1000 * timer / n_run for timer in timers]
+
 
 def main():
-    if len(sys.argv) >= 2:
-        try:
-            i = int(sys.argv[1])
-        except:
-            return "Argument is not a number"
+    args = get_args()
 
-        if f"day{i}.py" not in os.listdir():
-            return "This day does not exist"
-        else:
-            return compute(i)
+    allowed_days = set([file[:-3] for file in os.listdir() if re.match(r"^day.*\.py$" ,file)])
+    if args.days is None:
+        days_to_compute = allowed_days
     else:
-        return "Please pass a number corresponding to the day you want to execute"
+        possible_days = set(args.days + [f"day{x}" for x in args.days])
+        days_to_compute = possible_days.intersection(allowed_days)
+
+    print(f"""
+        Days to compute:\t{', '.join(days_to_compute)}
+        Compute solutions?\t{'✓' if args.solutions else '✘'}
+        Compute timings?\t{'✓' if args.timers else '✘'}
+    """)
+
+    output_ = {}
+    for day in days_to_compute:
+        solver, input_ = preload(day)
+        daily = {}
+
+        if args.solutions:
+            daily['solutions'] = solver(input_)
+
+        if args.timers:
+            daily['timers_ms'] = timers(solver, input_)
+
+        output_[day] = daily
+
+    print(output_)
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
